9035f89be7ec637f389c437b2fcc2dfe0bcb9006
Murali Krishna Kirthi <muralikrishna.kirthi@gmail.com>
2024-06-07 01:55:18 +0530

    ad

---
+++ Velocloud/9.vedge_resource.py
@@ -0,0 +1,39 @@
+'''
+Begin Declare Input Parameters
+ [
+ ]
+ End Declare
+ '''
+import time
+import json
+
+def BuildParameters(context, device_name, params):
+    node_dn = GetDeviceProperties(context, device_name, {'techName': 'VELOCLOUD', 'paramType': 'SDN', 'params':['id', 'nbPathSchema', 'nbPathValue', 'enterpriseId']})
+    #raise NameError (node_dn)
+    edge_id = node_dn['params'].get('id')
+    enterprise_id = node_dn['params'].get('enterpriseId')
+    #raise NameError (enterprise_id)
+    dev_schema = node_dn['params'].get('nbPathSchema')
+    rtn_params = { 'name':device_name, 'edge_id' : edge_id, 'enterprise_id':enterprise_id, 'dev_schema': dev_schema}
+    return rtn_params
+
+def RetrieveData(rtn_params):
+    edge_id = int(rtn_params['edge_id'])
+    enterprise_id = int(rtn_params['enterprise_id'])
+    dev_name = rtn_params['name']
+    if not (edge_id and enterprise_id):
+        return {'error:':'params not correct', 'rtn_params':rtn_params}
+    #edge_url = '/portal/rest/edge/getEdgeConfigurationModules'
+    
+    edge_url = '/portal/rest/metrics/getEdgeStatusMetrics'
+    
+    
+    body = {
+        "enterpriseId": enterprise_id,
+        "edgeId": edge_id,
+        "modules": ["deviceSettings"]
+    }
+    rtn_params['url'] = edge_url
+    rtn_params['body'] = body
+    data = json.loads(getData(rtn_params))
+    return json.dumps(data,indent = 4)
